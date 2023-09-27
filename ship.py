4fc64ebf2862c1c16425f1e4a96fa9571899e3a5
dac392 <dac392@scarletmail.rutgers.edu>
2023-09-27 16:37:13 -0400

    played around with the colors

--- ship.py
+++ ship.py
@@ -5,17 +5,21 @@ from generate_matrix import display_image
 class ship_t():
 	"""docstring for Ship_t"""
 	def __init__(self, size):
-		# super(ship_t, self).__init__()
 		self.size = size
 		self.game_board = generate_matrix(size)
 		# self.active_board = [row[:] for row in game_board]
 		self.bot = generate_random_coordinate(size)
 		self.button = generate_random_coordinate(size)
+		self.initial_fire = generate_random_coordinate(size)
+
+		#update the matrix to have the button and bot positions
+		self.game_board[self.bot] = 2
+		self.game_board[self.button] = 3
+		self.game_board[self.initial_fire] = 4
+
 		# prob generate a matrix for probabilities
 
 	def display_game_board(self):
-		self.game_board[self.bot] = 2
-		self.game_board[self.button] = 3
 		display_image(self.game_board)
 
 
