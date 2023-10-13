d94198a5afe09eff068d3780ac830feac8c52ce1
dac392 <dac392@scarletmail.rutgers.edu>
2023-10-13 19:49:12 -0400

    readme

--- data_collection.py
+++ data_collection.py
@@ -22,6 +22,7 @@ def data_collector():
 
 	# Create a list to hold all results
 	all_results = []
+	threads = []
 
 	Iterate over all grid sizes and flammabilities
 	for grid_size in range(20, 201+1, 5):  # From 20 to 200 in intervals of 5
@@ -30,23 +31,15 @@ def data_collector():
 
 			t = threading.Thread(target = run_simulation, args=(results_queue, grid_size, flammability))
 			t.start()
+			
+		for t in threads:
+			print("joined a thread. woo")
 			t.join()
 
-			# Collect results
-			while not results_queue.empty():
-				all_results.append(results_queue.get())
+		# Collect results
+		while not results_queue.empty():
+			all_results.append(results_queue.get())
 
-
-	# for i in range(10000):
-	# 	grid_size = 50
-	# 	flammability = 0.5
-	# 	t = threading.Thread(target = run_simulation, args=(results_queue, grid_size, flammability))
-	# 	t.start()
-	# 	t.join()
-
-	# 	# Collect results
-	# 	while not results_queue.empty():
-	# 		all_results.append(results_queue.get())
 
 
 	return all_results
@@ -62,11 +55,8 @@ def write_to_csv(data):
 
 
 def main():
-	# end_time = time.time() + 1*1*2  # 3 hours from now [hours]*[minutes]*[seconds]: 3*60*60 - 3 hours
-	# while time.time() < end_time:
-	# 	data = data_collector()
-	# 	write_to_csv(data)
-	for i in range(500):
+	for i in range(10):
+		print(f"running... {i}")
 		data = data_collector()
 		write_to_csv(data)
 
